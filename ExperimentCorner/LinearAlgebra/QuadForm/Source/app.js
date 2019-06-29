7ffe2b9e00a2e6a53d23230e61e87a28a1d2f0de
MrityunjayBhardwaj <mrityunjaybhardwaj99@gmail.com>
2019-06-29 14:07:52 +0530

    imporve UI

--- ExperimentCorner/LinearAlgebra/QuadForm/Source/app.js
+++ ExperimentCorner/LinearAlgebra/QuadForm/Source/app.js
@@ -1,10 +1,22 @@
 // TODO: make it async
 const matrix = tf.tensor( [[ -1,0],[0,0]] );
 
-function vizQuadSurf(matrix,res=10){
+
+const qSLayout = {
+    title: 'Quadratic Suface',
+    uirevision:'true',
+    xaxis: {autorange: true},
+    yaxis: {autorange: true},
+    zaxis: {autorange: true},
+    "titlefont": {
+    "size": 36
+  },
+}
+
+function vizQuadSurf(matrix,isNormalized=1,res=50,){
 
     // calculating quadratic form
-    const matrixQuadForm = quadForm(matrix,range={min: -1, max: 1},isNormalized=1,res); 
+    const matrixQuadForm = quadForm(matrix,range={min: -1, max: 1},isNormalized,res); 
 
     // console.log(matrixQuadForm)
     // visualizing QuadForm:-
@@ -14,15 +26,32 @@ function vizQuadSurf(matrix,res=10){
             y:matrixQuadForm.y,
             z:matrixQuadForm.z,
 
-            type: 'surface'
+            type: 'surface',
 
         }
     ]
 
     // plotting to the canvas
-    Plotly.newPlot('quadSurfViz',quadSurfVizData,{title: 'Quadratic Suface'});
+    Plotly.newPlot('quadSurfViz',quadSurfVizData,qSLayout);
 
+    const quadSurfHeatMapVizData = [
+        {
+            x: matrixQuadForm.x,
+            y: matrixQuadForm.y,
+            z: matrixQuadForm.z,
+            type: 'heatmap'
+        }
+    ]
+
+    Plotly.newPlot('quadSurfHeadMapViz',quadSurfHeatMapVizData,{margin: {pad:5,t:35,l:35},})
 }
+
+
+vizQuadSurf(tf.zeros([2,2]))
+
+
+let currMtx = [[0,0],[0,0]];
+
 
 // gathring all the sliders
 const slider00 = document.getElementById("range00");
@@ -34,6 +63,10 @@ const matrixVal00 = document.getElementById("matrixValue00");
 const matrixVal01 = document.getElementById("matrixValue01");
 const matrixVal10 = document.getElementById("matrixValue10");
 const matrixVal11 = document.getElementById("matrixValue11");
+
+const normToggle = document.getElementById("normalizedToggle");
+
+let normalizeOrNot = normToggle.value;
 
 // creating a matrix:-
 function onSliderChange(){
@@ -47,13 +80,23 @@ function onSliderChange(){
     matrixVal01.innerHTML = mtx01;
     matrixVal10.innerHTML = mtx10;
     matrixVal11.innerHTML = mtx11;
-    
+
+    normalizeOrNot = normToggle.checked;
+
     // upating the current matrix 
     currMtx = [[mtx00,mtx01],[mtx10,mtx11]];
     // console.log(currMtx)
     const tfMtx = tf.tensor(currMtx);
+
+    console.log(normalizeOrNot)
     // updating quad surf viz
-    vizQuadSurf(tfMtx);
+    vizQuadSurf(tfMtx,normalizeOrNot);
 }
 
 
+function onNormToggleChange(toggle){
+    // console.log(this.checked)
+    normalizeOrNot = normToggle.checked;
+    const tfMtx = tf.tensor(currMtx);
+    vizQuadSurf( tfMtx , normalizeOrNot);
+}

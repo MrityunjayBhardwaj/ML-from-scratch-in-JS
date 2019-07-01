87dcb102c7cfb2d6d04e59d5cbf721e78b6eac1e
MrityunjayBhardwaj <mrityunjaybhardwaj99@gmail.com>
2019-07-02 00:40:33 +0530

    debugged KNN

--- ExperimentCorner/KNN/Source/KNN.js
+++ ExperimentCorner/KNN/Source/KNN.js
@@ -1,6 +1,6 @@
 
 function KNN(){
-    model = {
+    const model = {
         params : {
             threshold : 0.4,
             k : 1
@@ -13,7 +13,7 @@ function KNN(){
      * @param { Number } threshold 
      * @param { Number } K number of neighbours 
      */
-    this.train = function(data,K){
+    this.train = function(data,K=1){
         const inputX = data.x;
         const inputY = data.y; 
 
@@ -28,32 +28,35 @@ function KNN(){
     this.classify = function(testDataX){
         // input must be a test set 
 
-        const categoryArray = testDataX.arraySync().map(
+        let categoryArray = testDataX.arraySync();
+        categoryArray = categoryArray.map(
             ( currPt ) => {
 
                 // init
                 const trainDataX = model.data.x;
+                const trainDataY = model.data.y;
                 const nNeigh     = model.params.k;
                 const tfCurrPt   = tf.tensor(currPt);
 
                 // calculating the norms.
                 const currPtNorm = tf.norm( tfCurrPt.transpose() );
                 const dataNorm   = tf.norm( trainDataX, 'euclidean',axis=1);
-                const normMatrix = tf.fill( currPtNorm.flatten().arraySync(), dataNorm.shape );
+                const normMatrix = tf.fill( dataNorm.shape, currPtNorm.flatten().arraySync()  );
 
                 // fetching the nearest data points
                 const nearnest = tf.pow( tf.sub( normMatrix, dataNorm ), 2 );
-                const neighY   = sortAB( nearnest.flatten().arraySync(), trainDataY );
+                const neighY   = tf.tensor( sortAB( nearnest.flatten().arraySync(), trainDataY )[1] );
 
                 // fectch the classes of K Nearest Neighbours
                 const kNeighClasses = neighY.slice([0,0],[nNeigh, -1]);
 
                 // calculating the relative frequency table of all the neighbour classes
-                const rf = kNeighClasses.transpose().matMul( tf.ones( [neighY.shape[0],1] ) );
-                const xCategory = rf.flatten().arraySync().indexOf(rf.max().flatten().arraySync());
+                const rf = kNeighClasses.transpose().matMul( tf.ones( [kNeighClasses.shape[0],1] ) );
+                const xCategory = rf.flatten().arraySync().indexOf( rf.max().flatten().arraySync()[0] );
                
                 // returns the one hot encoded vector of the best possible category
-                return rf[xCategory]; 
+                return kNeighClasses.slice([xCategory,0],[1,-1]).flatten().arraySync(); 
+
             }
         )
 
--- ExperimentCorner/KNN/Source/app.js
+++ ExperimentCorner/KNN/Source/app.js
@@ -4,7 +4,31 @@ const mIrisX = tf.tensor(iris).slice([0,0],[100,2])
 // one hot encoded
 const mIrisY = Array(100).fill([1,0],0,50).fill([0,1],50);
 
-let model = KNN();
+let augIrisData = mIrisX.concat(tf.tensor(mIrisY),axis=1).arraySync();
+tf.util.shuffle(augIrisData,axis=0);
+augIrisData = tf.tensor( augIrisData );
 
-model.train();
-model.test();
+const testX =   augIrisData.slice([0,0],[25,2]);
+const testY =   augIrisData.slice([0,2],[25,-1]);
+
+testX.print();
+testY.print();
+let model = new KNN();
+model.train({ x: mIrisX, y: mIrisY },K=5);
+const predY = model.classify(testX);
+
+
+
+const inputSpaceVizData = [{
+    x: mIrisX.slice([0,0],[50,-1]).slice([0,0],[-1,1]).arraySync(),
+    y: mIrisX.slice([50,0],[-1,-1]).slice([0,1],[-1,-1]).arraySync(),
+
+    mode: 'markers',
+    type: 'scatter',
+
+    marker : {
+        width: 5
+    }
+}]
+
+Plotly.newPlot('inputSpaceViz',inputSpaceVizData,{title: 'input Space'})

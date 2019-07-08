7e7b469d5a3894dda1b82a11e9cddbcfdb91de68
MrityunjayBhardwaj <mrityunjaybhardwaj99@gmail.com>
2019-07-08 21:26:16 +0530

    added projection method for linear algebra

---
+++ ExperimentCorner/LinearAlgebra/Projection/Source/projection.js
@@ -0,0 +1,10 @@
+
+/**
+ * 
+ * @param {object} A A is a subspace.
+ * @param {object} b b is a vector.
+ * @summary this function project vector 'b' onto a subspace 'A'.
+ * @returns returns the projected vector of 'b'.
+ */
+function project(A,b){
+}

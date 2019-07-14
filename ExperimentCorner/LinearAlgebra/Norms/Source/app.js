0c86fb713f706cad1236eab6771daac95e701424
MrityunjayBhardwaj <mrityunjaybhardwaj99@gmail.com>
2019-07-14 20:05:14 +0530

    added darkMode Color pallet

--- ExperimentCorner/LinearAlgebra/Norms/Source/app.js
+++ ExperimentCorner/LinearAlgebra/Norms/Source/app.js
@@ -1,4 +1,18 @@
 
+/* color schemes taken from :- https://codepen.io/ruchern/pen/OgJqvr */
+let darkModeCols = {
+	red:   (alpha = 1)=> `rgba(255, 99, 132,${alpha})`,
+	orange:(alpha = 1)=> `rgba(255, 159, 64,${alpha})`,
+	yellow:(alpha = 1)=> `rgba(255, 205, 86,${alpha})`,
+	green: (alpha = 1)=> `rgba(75, 192, 192,${alpha})`,
+	blue:  (alpha = 1)=> `rgba(54, 162, 235,${alpha})`,
+	purple:(alpha = 1)=> `rgba(153, 102, 255,${alpha})`,
+    grey:  (alpha = 1)=> `rgba(231,233,237,${alpha})`,
+    magenta: (alpha = 1) =>`rgba(255,0,255, ${alpha})`,
+    violet: (alpha = 1) =>`rgba(255,0,255, ${alpha})`
+};
+
+
 const x = tf.tensor([[1],[2]]);
 
 // generate meshgrid
@@ -10,19 +24,36 @@ const  g = pNorm(tf.tensor(tf.tensor(grid[0]).transpose().arraySync()[0]).expand
 
 const pNormGrid = grid.map( (a) =>{
     const f = tf.tensor(a).transpose().arraySync();
-    const w = f.map( (b) =>{ return 1*(pNorm(tf.tensor(b),p=1).flatten().arraySync()[0])});
+    const w = f.map( (b) =>{ 
+        return 1*(pNorm(tf.tensor(b),p=1).flatten().arraySync()[0] )
+    });
     // console.log(w);
     return w;
 });
 console.log('grid ',grid);
 console.log('pNormGrid ',pNormGrid);
-
+    
 // visualzing the pNorm Grid
 const pNormVizData = [{
     x : grid[0][0],
     y : grid[0][0],
     z : pNormGrid,
-    type: 'heatmap'
+    type: 'contour',
+    // colorscale: 'heatmap',
+    // colorscale: [[0, 'rgb(166,206,227)'], [0.25, 'rgb(31,120,180)'], [0.45, 'rgb(178,223,138)'], [0.65, 'rgb(51,160,44)'], [0.85, 'rgb(251,154,153)'], [1, 'rgb(227,26,28)']],
+
+    colorscale : [[0, darkModeCols.blue()], [0.25, darkModeCols.purple()],[0.5, darkModeCols.magenta()], [.75, darkModeCols.yellow()], [1, darkModeCols.red()]],
+
+    // mode: 'heatmap'
+    contours: {
+        // coloring: 'heatmap',
+     },
+    line : {
+        // width: 0
+    }
 }];
 
-Plotly.newPlot('pNormViz',pNormVizData,{title: 'P-Norm'})
+Plotly.newPlot('pNormViz',pNormVizData,{title: 'P-Norm'});
+
+
+

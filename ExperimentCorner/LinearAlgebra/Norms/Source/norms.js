9b9e59d0c393c58e247b365197c32bbf40f8e259
MrityunjayBhardwaj <mrityunjaybhardwaj99@gmail.com>
2019-08-01 22:00:07 +0530

    added mtxNorm

--- ExperimentCorner/LinearAlgebra/Norms/Source/norms.js
+++ ExperimentCorner/LinearAlgebra/Norms/Source/norms.js
@@ -9,6 +9,10 @@ function pNorm(x,p=1){
     return tf.pow(tf.sum(tf.pow(tf.abs(x), p), axis=0), 1/p);
 }
 
+function mtxNorm(A){
+    return tf.sqrt( tf.sum( tf.abs(A).pow(2) ) );
+}
+
 function inducedMatrixNorm(A,x,p){
     return tf.div(pNorm(tf.matMul(A, x), p ), 1 );
 }

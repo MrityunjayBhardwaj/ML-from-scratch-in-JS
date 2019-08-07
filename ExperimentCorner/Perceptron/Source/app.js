d59761d0f87a2341edd4c7bfc5171b009914ce9d
MrityunjayBhardwaj <mrityunjaybhardwaj99@gmail.com>
2019-08-07 21:17:41 +0530

    quick fix

--- ExperimentCorner/Perceptron/Source/app.js
+++ ExperimentCorner/Perceptron/Source/app.js
@@ -35,26 +35,26 @@ function perceptronViz(){
 
     const perceptronVizData = [
         {
-            x : mIrisX.slice([0, -1],[50, -1]).slice([0, 0],[-1,  1]).flatten().arraySync(),
-            y : mIrisX.slice([0, -1],[50, -1]).slice([0, 1],[-1, -1]).flatten().arraySync(),
+            x : mIrisX.slice([0, 0],[50, -1]).slice([0, 0],[-1,  1]).flatten().arraySync(),
+            y : mIrisX.slice([0, 0],[50, -1]).slice([0, 1],[-1, -1]).flatten().arraySync(),
             mode : 'markers',
             type : 'scatter',
 
         },
         {
-            x : mIrisX.slice([50, -1],[-1, -1]).slice([0, 0],[-1,  1]).flatten().arraySync(),
-            y : mIrisX.slice([50, -1],[-1, -1]).slice([0, 1],[-1, -1]).flatten().arraySync(),
+            x : mIrisX.slice([50, 0],[-1, -1]).slice([0, 0],[-1,  1]).flatten().arraySync(),
+            y : mIrisX.slice([50, 0],[-1, -1]).slice([0, 1],[-1, -1]).flatten().arraySync(),
             mode : 'markers',
             type : 'scatter',
 
         },
-        {
-            x : [-decisionBoundary[0], decisionBoundary[1],],
-            y : [-decisionBoundary[0], decisionBoundary[1],],
-            mode : 'lines',
-            type : 'scatter'
+        // {
+        //     x : [-decisionBoundary[0], decisionBoundary[1],],
+        //     y : [-decisionBoundary[0], decisionBoundary[1],],
+        //     mode : 'lines',
+        //     type : 'scatter'
 
-        }
+        // }
     ];
 
     Plotly.newPlot('perceptronViz', perceptronVizData, {title: 'Rosenblatt\'s perceptron' });
@@ -62,3 +62,52 @@ function perceptronViz(){
 }
 
 perceptronViz();
+
+
+
+// generate meshgrid
+const grid = meshGridRange(range={x: {min: -2.1, max: +2.1},y: {min: -2.1, max: +2.1}},division=20);
+
+// calculate pNorm for each value of meshgrid
+
+const pNormGrid = grid.map( (a) =>{
+    const f = tf.tensor(a).transpose().arraySync();
+    const w = f.map( (b) =>{ 
+        // return 1*(pNorm(tf.tensor(b).expandDims(1), p=2).flatten().arraySync()[0] )
+        return 1*model.test(tf.tensor(b).expandDims(1).transpose()).flatten().arraySync()[0];
+        // return 1*(inducedMatrixNorm(tf.tensor([[1, 2],[0, 2]]),tf.tensor(b).expandDims(1),p=2).flatten().arraySync()[0] )
+    });
+    return w;
+});
+
+// visualzing the pNorm Grid
+const pNormVizData = [{
+    x : grid[0][0],
+    y : grid[0][0],
+    z : pNormGrid,
+    type: 'contour',
+
+    colorscale : [[0, darkModeCols.blue()], [0.25, darkModeCols.purple()],[0.5, darkModeCols.magenta()], [.75, darkModeCols.yellow()], [1, darkModeCols.red()]],
+
+    contours: {
+        start: -1,
+        end: 1,
+        size: 3
+     },
+    line : {
+    },
+}];
+
+const layoutSetting = {
+    title : 'p-Norm',
+    font : {
+        size : 15,
+        color: 'white',
+        family : 'Helvetica'
+    },
+    paper_bgcolor : '#222633',
+
+
+}
+
+Plotly.newPlot('decisionBoundaryViz',pNormVizData,layoutSetting);

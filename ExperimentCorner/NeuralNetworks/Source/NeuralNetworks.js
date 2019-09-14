bfb06a4c72e1bd889a265b95428de91804bd42eb
MrityunjayBhardwaj <mrityunjaybhardwaj99@gmail.com>
2019-09-14 21:06:49 +0530

    added forwardPass to our neuralNetwork

--- ExperimentCorner/NeuralNetworks/Source/NeuralNetworks.js
+++ ExperimentCorner/NeuralNetworks/Source/NeuralNetworks.js
@@ -3,13 +3,13 @@
 function NeuralNetworks(){
     this.model = {
         params : {
-            weights: {
-
-            }
 
         },
+        weights: tf.tensor(),
 
     }
+
+    this.getWeights = function(){return model.weights}
 
     this.derivatives = function() {
 
@@ -21,6 +21,44 @@ function NeuralNetworks(){
 
     this.train = function(data){
         
+
+
+    }
+
+    this.forwardPass = function(data, weights = null){
+
+        const dataX = data.x;
+        const dataY = data.y;
+
+        const weights = weights || this.getWeights();
+
+        if (weights.shape[0] === 0){
+            throw new Error('No weights specified, either specify the weights or train the model');
+        }
+
+        // if we have weights :-
+
+        const nLayers = model.params.networkStruct.length; // no of layers:- hidden + 1+outputlayer
+
+        let preLayer = dataX;
+
+        let output = 0;
+        for(let i=1; i<nLayers; i++){
+
+            const cWeights = weights.slice([0,0], [-1, i]);
+
+
+            const prepro = preLayer.matMul( cWeights );
+            // TODO: Add functionality to have different activation function. for each neuron
+            const activation = this.activationFn(prepro);
+
+            if(i++ === nLayers)
+                output = activation;
+        }
+
+        // calculate the error:-
+
+        this.lossFn(output, dataY)
 
 
     }

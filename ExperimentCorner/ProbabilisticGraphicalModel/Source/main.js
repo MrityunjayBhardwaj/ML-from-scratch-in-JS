71f416460d1c5113ffc4adf3afd8b0bf39f5145a
MrityunjayBhardwaj <mrityunjaybhardwaj99@gmail.com>
2019-12-03 15:51:28 +0530

    started main.js

---
+++ ExperimentCorner/ProbabilisticGraphicalModel/Source/main.js
@@ -0,0 +1,16 @@
+
+/**
+ * TODO Factor Graphs and inference:
+ * * create a simple interface for formulating a graph ( directed or undirected)
+ * * create a function which converts the directed into an undirected graph
+ * * create a d-seperation function which converts ann undirected graph into d-seperated graph.
+ * * create a function that converts undirected graph into factor graph.
+ * * use this factor graph to run inference algorithms
+ * * implement sum product function to perform exact inference on the given singly connected factor graph.
+ * * implement junction-tree algorithm to perform approximate inference on the graphs with loops.
+ */
+
+// Graph is a adjecency matrix
+function dSeperation(Graph){
+
+}
